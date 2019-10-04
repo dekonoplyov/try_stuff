7bd7e217c43a085dc39e41a619be04babe396880
Denis Konoplev <denis.konoplev@jetbrains.com>
2019-10-04 18:39:48 +0300

    Cmake & initialization

--- win_man/window_manager.cpp
+++ win_man/window_manager.cpp
@@ -9,9 +9,9 @@ std::unique_ptr<WindowManager> WindowManager::create()
     auto display = XOpenDisplay(nullptr);
     if (display == nullptr) {
         LOG(ERROR) << "Failed to open XDisplay: " << XDisplayName(nullptr);
-        return nullptr;
     }
-    return std::make_unique<WindowManager>(display);
+    return nullptr;
+    // return std::make_unique<WindowManager>(display);
 }
 
 WindowManager::WindowManager(Display* d)
@@ -25,8 +25,68 @@ WindowManager::~WindowManager()
     XCloseDisplay(display_);
 }
 
-void run()
+void WindowManager::run()
 {
+    // Select events on root window. Use a special error handler so we can
+    // exit gracefully if another window manager is already running.
+    wm_detected_ = false;
+    XSetErrorHandler(&WindowManager::onWMDetected);
+    XSelectInput(display_, root_,
+        SubstructureRedirectMask | SubstructureNotifyMask);
+    XSync(display_, false);
+    if (wm_detected_) {
+        LOG(ERROR) << "Detected another window manager on display "
+                   << XDisplayString(display_);
+        return;
+    }
+
+    // set general error handler
+    XSetErrorHandler(&WindowManager::onXError);
+
+    while (true) {
+        XEvent e;
+        XNextEvent(display_, &e);
+
+        // LOG(INFO) << "Received event: " << ToString(e);
+
+        switch (e.type) {
+        case CreateNotify:
+            /* code */
+            break;
+        case DestroyNotify:
+            /* code */
+            break;
+        case ReparentNotify:
+            /* code */
+            break;
+
+        default:
+            LOG(WARNING) << "Ignored event";
+            break;
+        }
+        /* code */
+    }
+}
+
+int WindowManager::onWMDetected(Display* d, XErrorEvent* e)
+{
+    (void)d;
+    // In the case of an already running window manager, the error code from
+    // XSelectInput is BadAccess. We don't expect this handler to receive any
+    // other errors.
+    CHECK_EQ(static_cast<int>(e->error_code), BadAccess);
+    // Set flag.
+    WindowManager::wm_detected_ = true;
+    // The return value is ignored.
+    return 0;
+}
+
+int WindowManager::onXError(Display* d, XErrorEvent* e)
+{
+    (void)e;
+    (void)d;
+    // Print error
+    return 0;
 }
 
 } // namespace win_man
--- win_man/window_manager.h
+++ win_man/window_manager.h
@@ -13,6 +13,14 @@ public:
     // establish connection to XServer and create WM instance
     static std::unique_ptr<WindowManager> create();
 
+    // general error handler
+    static int onXError(Display* d, XErrorEvent* e);
+    // error hander to determine whether another window manager is
+    // running. It is set as the error handler right before selecting substructure
+    // redirection mask on the root window, so it is invoked if and only if
+    // another window manager is running.
+    static int onWMDetected(Display* d, XErrorEvent* e);
+
     // ~disconect from XServer
     ~WindowManager();
 
@@ -23,6 +31,8 @@ private:
     WindowManager(Display* d);
 
 private:
+    static bool wm_detected_;
+    
     Display* display_;
     const Window root_;
 };
